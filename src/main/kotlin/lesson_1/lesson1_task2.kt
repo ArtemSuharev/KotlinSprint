8ef1201cfc81bbd927e9db585099bb89b3ee285f
ArtemSuharev <114438648+ArtemSuharev@users.noreply.github.com>
2024-06-05 10:06:40 +0300

    KS-1-2 Number of employees

--- src/main/kotlin/lesson_1/lesson1_task2.kt
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -1,2 +1,19 @@
 package org.example.lesson_1
 
+fun main() {
+
+    val numberOfOrder: Int = 75
+    val textOfThankfulness: String = "Спасибо, что совершили покупку в нашем магазине!"
+
+    println(numberOfOrder)
+    println(textOfThankfulness)
+
+    var numberOfEmployee = 2000
+
+    //println(numberOfEmployee)
+
+    numberOfEmployee -= 1
+
+    println(numberOfEmployee)
+
+}
