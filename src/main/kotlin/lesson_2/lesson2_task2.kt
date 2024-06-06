166b94404b08f02c9a6fdad7fd206c5d274c2e56
ArtemSuharev <114438648+ArtemSuharev@users.noreply.github.com>
2024-06-06 11:09:19 +0300

    KS-2-2 Salary (correct)

--- src/main/kotlin/lesson_2/lesson2_task2.kt
+++ src/main/kotlin/lesson_2/lesson2_task2.kt
@@ -1,15 +1,15 @@
 package org.example.lesson_2
 
-const val SALARY_OF_EMPLOYEES = 30000
-const val SALARY_OF_INTERNS = 20000
-
 fun main() {
+
+    val salaryOfEmployees = 30000
+    val salaryOfInterns = 20000
 
     val countOfEmployees = 50
     val countOfInterns = 30
 
-    val totalSalaryEmployees = countOfEmployees * SALARY_OF_EMPLOYEES
-    val totalSalaryEmployeesAndInterns = totalSalaryEmployees + countOfInterns * SALARY_OF_INTERNS
+    val totalSalaryEmployees = countOfEmployees * salaryOfEmployees
+    val totalSalaryEmployeesAndInterns = totalSalaryEmployees + countOfInterns * salaryOfInterns
     val averageSalary = totalSalaryEmployeesAndInterns / (countOfInterns + countOfEmployees)
 
     println("Расходы на выплату зарплаты постоянных сотрудников: $totalSalaryEmployees руб.")
