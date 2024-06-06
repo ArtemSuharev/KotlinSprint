32188aeb12aeac6cb7fdda03e835a7c1c812529d
ArtemSuharev <114438648+ArtemSuharev@users.noreply.github.com>
2024-06-06 11:59:20 +0300

    KS-2-4 Buff-bonus (correct)

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -2,15 +2,17 @@ package org.example.lesson_2
 
 import kotlin.math.roundToInt
 
-const val BUFF = 0.2
-
 fun main() {
+
+    val buffPercentage = 20
 
     val countCrystalOre = 7
     val countIronOre = 11
 
-    val buffCrystalOre : Int = (countCrystalOre * BUFF).roundToInt()
-    val buffIronOre : Int = (countIronOre * BUFF).roundToInt()
+    val buff : Double = buffPercentage.toDouble() / 100
+
+    val buffCrystalOre : Int = (countCrystalOre * buff).roundToInt()
+    val buffIronOre : Int = (countIronOre * buff).roundToInt()
 
     println("Количество бонусных кристаллических руд - $buffCrystalOre шт.")
     println("Количество бонусных железный руд - $buffIronOre шт.")
