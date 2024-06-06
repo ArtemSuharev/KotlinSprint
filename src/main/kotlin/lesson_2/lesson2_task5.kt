26aebe006087f2d15221712fdfabdf5b119a4d4e
ArtemSuharev <114438648+ArtemSuharev@users.noreply.github.com>
2024-06-06 12:07:10 +0300

    KS-2-5 Count of deposit (correct)

--- src/main/kotlin/lesson_2/lesson2_task5.kt
+++ src/main/kotlin/lesson_2/lesson2_task5.kt
@@ -2,14 +2,15 @@ package org.example.lesson_2
 
 import kotlin.math.pow
 
-const val PERCENT_PER_YEAR = 0.167
-
 fun main() {
+
+    val percentagePerYear : Double = 16.7
 
     val countOfMoney = 70_000
     val countOfYears = 20
+    val percentageValue : Double = percentagePerYear / 100
 
-    val countOfDeposit = countOfMoney * (1 + PERCENT_PER_YEAR).pow(countOfYears)
+    val countOfDeposit = countOfMoney * (1 + percentageValue).pow(countOfYears)
     println("Размер вклада через $countOfYears лет = ${ "%.3f".format(countOfDeposit) } руб.")
 
 }
