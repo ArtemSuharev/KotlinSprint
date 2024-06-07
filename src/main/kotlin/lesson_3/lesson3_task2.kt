a9f01ee8f9fd7039411402c43be3c5250d2e98cf
ArtemSuharev <114438648+ArtemSuharev@users.noreply.github.com>
2024-06-07 08:54:20 +0300

    KS-3-2 Change of surname after the wedding (correct)

--- src/main/kotlin/lesson_3/lesson3_task2.kt
+++ src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -2,16 +2,17 @@ package org.example.lesson_3
 
 fun main() {
 
-    val wifeSurname = "Андреева"
+    var wifeSurname = "Андреева"
     val wifeName = "Татьяна"
     val wifePatronymic = "Сергеевна"
-
-    val wifeSurnameChanged = "Сидорова"
 
     val ageBeforeMarriage = 20
     val ageAfterMarriage = 22
 
     println("[$wifeSurname $wifeName $wifePatronymic, $ageBeforeMarriage]")
-    println("[$wifeSurnameChanged $wifeName $wifePatronymic, $ageAfterMarriage]")
+
+    wifeSurname = "Сидорова"
+
+    println("[$wifeSurname $wifeName $wifePatronymic, $ageAfterMarriage]")
 
 }
