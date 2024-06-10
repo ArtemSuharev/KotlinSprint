615599c3aa597c9f14849cffba3d007a929059e0
ArtemSuharev <114438648+ArtemSuharev@users.noreply.github.com>
2024-06-10 10:25:35 +0300

    KS-4-3 Season's state (correct)

--- src/main/kotlin/lesson_4/lesson4_task3.kt
+++ src/main/kotlin/lesson_4/lesson4_task3.kt
@@ -1,18 +1,18 @@
 package org.example.lesson_4
 
-const val GOOD_TERMS_DAY = "солнечная"
-const val GOOD_TERMS_TENT = "открыт"
+const val IS_SUNNY = true
+const val IS_AWNING_OPEN = true
 const val GOOD_TERMS_HUMIDITY = 20
 const val GOOD_TERMS_SEASON = "зима"
 
 fun main() {
 
-    val isSunnyDay = "солнечная"
-    val isOpenedTent = "открыт"
+    val isSunnyDay = true
+    val isOpenedTent = true
     val humidity = 20
     val season = "зима"
 
-    val condition = (isSunnyDay == GOOD_TERMS_DAY) && (isOpenedTent == GOOD_TERMS_TENT) && (humidity == GOOD_TERMS_HUMIDITY) && (season != GOOD_TERMS_SEASON)
+    val condition = (isSunnyDay == IS_SUNNY) && (isOpenedTent == IS_AWNING_OPEN) && (humidity == GOOD_TERMS_HUMIDITY) && (season != GOOD_TERMS_SEASON)
 
     println("Благоприятные ли условия сейчас для роста бобовых? $condition")
 
