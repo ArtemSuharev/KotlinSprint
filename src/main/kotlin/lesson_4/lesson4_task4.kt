a99ad8168e795d3904eb6c1dc0e6b1aec17a113f
ArtemSuharev <114438648+ArtemSuharev@users.noreply.github.com>
2024-06-10 10:29:55 +0300

    KS-4-4 Workout (correct)

--- src/main/kotlin/lesson_4/lesson4_task4.kt
+++ src/main/kotlin/lesson_4/lesson4_task4.kt
@@ -2,13 +2,14 @@ package org.example.lesson_4
 
 fun main() {
 
-    val numberDay = 5
+    val numberDay = 2
+    val isEvenDay = (numberDay % 2 == 0)
 
     println("""
-        Упражнения для рук: ${numberDay % 2 == 1}
-        Упражнения для ног: ${numberDay % 2 == 0}
-        Упражнения для спины: ${numberDay % 2 == 0}
-        Упражнения для пресса: ${numberDay % 2 == 1}
+        Упражнения для рук: ${!isEvenDay}
+        Упражнения для ног: $isEvenDay
+        Упражнения для спины: $isEvenDay
+        Упражнения для пресса: ${!isEvenDay}
     """.trimIndent())
 
 }
