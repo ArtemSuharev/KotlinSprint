ffb26185976edbafdb0882be629a59da1f9eb739
ArtemSuharev <timburtag@gmail.com>
2024-06-12 17:23:39 +0300

    KS-4-5 Ship's sail (correct)

--- src/main/kotlin/lesson_4/lesson4_task5.kt
+++ src/main/kotlin/lesson_4/lesson4_task5.kt
@@ -4,10 +4,12 @@ const val MIN_CREW_COUNT = 55
 const val MAX_CREW_COUNT = 70
 const val MIN_BOX_COUNT = 50
 
+const val YES = "да"
+
 fun main() {
 
     println("Наличие повреждений корпуса (да/нет):")
-    val isDamage: Boolean = (readln() == "да")
+    val isDamage: Boolean = (readln().lowercase() == YES)
 
     println("Текущий состав экипажа (количество человек):")
     val crewCount: Int = readln().toInt()
@@ -16,13 +18,13 @@ fun main() {
     val boxCount: Int = readln().toInt()
 
     println("Благоприятность метеоусловий (да/нет):")
-    val isGoodWeather: Boolean = (readln() == "да")
+    val isGoodWeather: Boolean = (readln().lowercase() == YES)
 
-    val isShipFlight =
+    val isShipSail =
         (!isDamage && (crewCount in MIN_CREW_COUNT..MAX_CREW_COUNT) && boxCount > MIN_BOX_COUNT) ||
                 (isGoodWeather && crewCount == MAX_CREW_COUNT && boxCount >= MIN_BOX_COUNT)
 
-    if (isShipFlight) {
+    if (isShipSail) {
         println("Корабль может отправиться в плавание")
     } else {
         println("Корабль не может отправиться в плавание")
